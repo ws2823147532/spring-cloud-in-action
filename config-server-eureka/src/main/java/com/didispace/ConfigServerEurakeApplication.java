b3b3b7b7099869bd6c7872b41c9d16ec2d5128e2
ws <2823147532@qq.com>
2017-08-14 18:18:04 +0800

    add

--- config-server-eureka/src/main/java/com/didispace/ConfigServerEurakeApplication.java
+++ config-server-eureka/src/main/java/com/didispace/ConfigServerEurakeApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class Application {
+public class ConfigServerEurakeApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		new SpringApplicationBuilder(ConfigServerEurakeApplication.class).web(true).run(args);
 	}
 
 }
